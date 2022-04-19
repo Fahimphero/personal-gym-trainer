893b0be5f205c70821035f6bc8b17d8df55975a9
unknown <irfanulislamfahim@gmail.com>
2022-04-19 22:06:27 +0600

    footer added

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-center mt-5' style={{ display: 'flex', flexDirection: 'column' }}>
-            <h6>Copyright © 2022 || <span style={{ color: 'blue' }}>personal-gym-trainer-61c76.web.app</span> </h6>
-            <h6 className='mt-1'>Khulna, Bangladesh</h6>
-            <small className='mt-1 mb-5'>All Rights Reserved</small>
+        <div className='text-center mt-5 bg-dark' style={{ display: 'flex', flexDirection: 'column' }}>
+            <h6 className='text-light pt-5'>Copyright © 2022 || <span style={{ color: 'blue' }}>personal-gym-trainer-61c76.web.app</span> </h6>
+            <h6 className='mt-1 text-light'>Khulna, Bangladesh</h6>
+            <small className='mt-1 mb-5 text-light'>All Rights Reserved</small>
         </div>
     );
 };
